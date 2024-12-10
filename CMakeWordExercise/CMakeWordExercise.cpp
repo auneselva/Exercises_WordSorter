d72e89ba99428c4126dde959d69a8c275b6ff1bc
Connie <agnieszkakonopka44@gmail.com>
2024-12-10 01:51:56 +0100

    Changed objects in main from stack to heap-allocated

--- CMakeWordExercise/CMakeWordExercise.cpp
+++ CMakeWordExercise/CMakeWordExercise.cpp
@@ -6,19 +6,19 @@
 
 int main()
 {
-	std::shared_ptr<Test> test = std::make_shared<Test>();
-	std::shared_ptr < std::unordered_map<std::string, uint32_t>> map = std::make_shared < std::unordered_map<std::string, uint32_t>>();
+	Test test;
+	std::unordered_map<std::string, uint32_t> map;
 	std::cout << "Hello, I'm a word sorter.\n\n" << std::endl;
 
 	Timer timer;
-	for (std::string example : test->examples) {
+	for (std::string example : test.examples) {
 		timer.Reset();
 
 		std::cout << "Example 0: |" << example << "|\n\n";
-		WordSorter::CountWords(example, *map);
-		WordSorter::SortWordsByFrequency(*map);
-		WordSorter::SortWordsAlphabetically(*map);
-		map->clear();
+		WordSorter::CountWords(example, map);
+		WordSorter::SortWordsByFrequency(map);
+		WordSorter::SortWordsAlphabetically(map);
+		map.clear();
 
 		std::cout << "\nElapsedMilis: " << timer.ElapsedMillis() << "ms. \n\n";
 	}
